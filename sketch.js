2d9cc5c3abe067efe8e339275d5043f0bbbfa256
Dharmik Santhosh Raghav <70014740+dharmiksanthosh@users.noreply.github.com>
2020-11-05 20:53:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,98 @@
+var dev,database;
+
+function setup() {
+
+  createCanvas(windowWidth,windowHeight);
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    
+    dev = "mobile";
+  }else{
+
+    dev = "pc";
+  }
+}
+
+function draw() {
+
+  background(0);
+  database = firebase.database();
+
+  if (dev==="mobile") {
+    
+    var u,l,d,r,xref,yref;
+
+    xref = database.ref('x');
+    xref.on("value",(data) => {x = data.val();});
+
+    yref = database.ref('y');
+    yref.on("value",(data) => {y = data.val();});
+
+    u = createButton("Up"); 
+    u.size(width/4,height/4);
+    u.position((width/2)-(u.width/2),(height/4)-(u.height/2));
+
+    d = createButton("Down"); 
+    d.size(width/4,height/4);
+    d.position((width/2)-(d.width/2),(height/4*3)-(d.height/2));
+
+    l = createButton("Left"); 
+    l.size(width/4,height/4);
+    l.position((width/4)-(l.width/2),(height/2)-(l.height/2));
+
+    r = createButton("Right"); 
+    r.size(width/4,height/4);
+    r.position((width/4*3)-(r.width/2),(height/2)-(r.height/2));
+
+      u.mousePressed(() => {
+
+        y -= 10;
+
+        database.ref('/').update({
+
+          y: y
+        });
+      });
+      d.mousePressed(() => {
+
+        y += 10;
+        
+        database.ref('/').update({
+
+          y: y
+        });
+      });
+      r.mousePressed(() => {
+
+        x += 10;
+
+        database.ref('/').update({
+  
+          x: x
+        });
+      });
+
+      l.mousePressed(() => {
+
+        x -= 10;
+
+        database.ref('/').update({
+  
+          x: x
+        });
+      });
+
+  }else {
+   
+    var x = -500,y = 0,xref,yref;
+
+    xref = database.ref('x');
+    xref.on("value",(data) => {x = data.val();});
+
+    yref = database.ref('y');
+    yref.on("value",(data) => {y = data.val();});
+
+    fill("red");
+    rectMode(CENTER);
+    rect(x,y,100,100);
+  }
+}
